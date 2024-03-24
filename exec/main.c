dcad34ac1f68fbd8fe4be561ffb9480da5d7287a
DawnteaStudio <dawnteabro@naver.com>
2024-03-24 18:32:25 +0900

    [Feat] add readline lib

--- exec/main.c
+++ exec/main.c
@@ -6,16 +6,22 @@
 /*   By: sewopark <sewopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 14:12:05 by sewopark          #+#    #+#             */
-/*   Updated: 2024/03/24 17:42:35 by sewopark         ###   ########.fr       */
+/*   Updated: 2024/03/24 18:10:32 by sewopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
-	(void)argc;
-	(void)argv;
-	printf("test");
+	char	*str;
+
+	((void)argc, (void)argv, (void)envp);
+	while (1)
+	{
+		str = readline("jetty_shell>");
+		printf("%s\n", str);
+		free(str);
+	}
 	return (0);
 }
