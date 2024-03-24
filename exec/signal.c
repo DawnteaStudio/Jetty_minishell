fdb37893b7cd8e259738caf3086a707ff9b4bdd5
DawnteaStudio <dawnteabro@naver.com>
2024-03-24 20:46:45 +0900

    [Feat] signal preview

--- exec/signal.c
+++ exec/signal.c
@@ -1,32 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sewopark <sewopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/06 14:12:05 by sewopark          #+#    #+#             */
-/*   Updated: 2024/03/24 20:44:53 by sewopark         ###   ########.fr       */
+/*   Created: 2024/03/24 20:11:28 by sewopark          #+#    #+#             */
+/*   Updated: 2024/03/24 20:43:58 by sewopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+void	sigint_handler(int _num)
 {
-	char			*str;
-	struct termios	term;
-
-	((void)argc, (void)argv, (void)envp);
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGINT, sigint_handler);
-	while (1)
-	{
-		str = readline("jetty_shell>");
-		if (str == NULL)
-			break ;
-		add_history(str);
-		free(str);
-	}
-	return (0);
+	(void)_num;
+	ft_putstr_fd("\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
 }
