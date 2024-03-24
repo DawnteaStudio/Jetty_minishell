98bda57a905aa055cdd6f2af95293572f3994114
DawnteaStudio <dawnteabro@naver.com>
2024-03-24 22:37:30 +0900

    [Feat] bool macro and some prototype

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sewopark <sewopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 11:18:31 by sewopark          #+#    #+#             */
-/*   Updated: 2024/03/24 20:43:30 by sewopark         ###   ########.fr       */
+/*   Updated: 2024/03/24 22:21:51 by sewopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,15 @@
 # include <signal.h>
 # include <termios.h>
 
+# define TRUE	1
+# define FALSE	0
+
 //signal
 void	sigint_handler(int _num);
+
+//built_in
+int		is_builtin(char *cmd);
+int		ft_pwd(void);
+
 
 #endif
