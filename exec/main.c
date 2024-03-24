b88049271836a9f128cded32a42fad2dcb905413
DawnteaStudio <dawnteabro@naver.com>
2024-03-24 17:46:21 +0900

    [Fix] makefile path

--- exec/main.c
+++ exec/main.c
@@ -6,7 +6,7 @@
 /*   By: sewopark <sewopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 14:12:05 by sewopark          #+#    #+#             */
-/*   Updated: 2024/03/06 20:01:29 by sewopark         ###   ########.fr       */
+/*   Updated: 2024/03/24 17:42:35 by sewopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	printf("test");
 	return (0);
 }
