f06cfb41dcc37b031fa3bbba74c30e713a2512da
eulrho <sunriselss19@gmail.com>
2024-04-04 18:01:07 +0900

    [Fix] improve angle bracket token

--- parse/parse_helper.c
+++ parse/parse_helper.c
@@ -6,7 +6,7 @@
 /*   By: erho <erho@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 22:17:11 by erho              #+#    #+#             */
-/*   Updated: 2024/04/04 15:35:21 by erho             ###   ########.fr       */
+/*   Updated: 2024/04/04 17:54:21 by erho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,21 @@ int	is_quote(char c)
 	return (1);
 }
 
+int	is_redirection(char c)
+{
+	if (c != '>' && c != '<')
+		return (0);
+	return (1);
+}
+
 int check_redirection(char *str, t_command *cmd)
 {
-	if (cmd->width >= 1 
-			&& str[cmd->width - 1] != '<' && str[cmd->width - 1] != '>')
-	{
-		if (str[cmd->width] == '<' || str[cmd->width] == '>')
-			return (1);
-	}
-	if ((str[cmd->width] != '<' && str[cmd->width] != '>') || cmd->len == 2)
+	if (cmd->width >= 1 && !is_redirection(str[cmd->width - 1]))
+		return (is_redirection(str[cmd->width]));
+	if (!is_redirection(str[cmd->width]))
+		return (1);
+	if ((cmd->width >= 1 && str[cmd->width - 1] != str[cmd->width])
+			|| cmd->len == 2)
 		return (1);
 	return (0);
 }
