57636cb2cf0e65753e9c75be3d3beddb8bd2448f
eulrho <sunriselss19@gmail.com>
2024-04-11 23:19:53 +0900

    [Feat] add tree node free func

--- parse/parse_helper.c
+++ parse/parse_helper.c
@@ -6,7 +6,7 @@
 /*   By: erho <erho@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 22:17:11 by erho              #+#    #+#             */
-/*   Updated: 2024/04/11 21:27:55 by erho             ###   ########.fr       */
+/*   Updated: 2024/04/11 22:21:32 by erho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,42 @@ void	free_tokens(t_token *tokens)
 		i++;
 	}
 	free(tokens);
+}
+
+void	free_node(t_tree **tree)
+{
+	int	idx;
+
+	if ((*tree)->cmd != NULL)
+		free((*tree)->cmd);
+	if ((*tree)->redir != NULL)
+		free((*tree)->redir);
+	if ((*tree)->redir_info != NULL)
+		free((*tree)->redir_info);
+	if ((*tree)->exp != NULL)
+	{
+		idx = 0;
+		while ((*tree)->exp[idx] != NULL)
+		{
+			free((*tree)->exp[idx]);
+			idx++;
+		}
+		free((*tree)->exp);
+	}
+	free(*tree);
+	*tree = NULL;
+}
+
+void	free_tree(t_tree **tree)
+{
+	t_tree	*t_left;
+	t_tree	*t_right;
+
+	t_left = (*tree)->left;
+	t_right = (*tree)->right;
+	free_node(tree);
+	if ((*tree)->left != NULL)
+		free_tree(&t_left);
+	if ((*tree)->right != NULL)
+		free_tree(&t_right);
 }
