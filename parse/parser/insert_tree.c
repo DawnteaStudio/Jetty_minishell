ffc063c43134b5b2432d4c3b011f3f41fef098c7
eulrho <sunriselss19@gmail.com>
2024-05-06 19:04:56 +0900

    [Feat] syntax error message

--- parse/parser/insert_tree.c
+++ parse/parser/insert_tree.c
@@ -6,11 +6,26 @@
 /*   By: erho <erho@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 11:05:09 by erho              #+#    #+#             */
-/*   Updated: 2024/05/06 17:03:34 by erho             ###   ########.fr       */
+/*   Updated: 2024/05/06 18:57:19 by erho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+int	redir_error(int flag, t_token tokens)
+{
+	ft_putstr_fd("jetty: syntax error near unexpected token ", 2);
+	if (tokens.type == TOKEN_TYPE_NULL)
+		ft_putstr_fd("'newline'\n", 2);
+	else
+	{
+		ft_putstr_fd("'", 2);
+		ft_putstr_fd(tokens.str, 2);
+		ft_putstr_fd("'\n", 2);
+	}
+	g_exit_code = 258;
+	return (flag);
+}
 
 void	command_node(t_tree **tree, t_token *tokens, t_env_node **env_list,
 		int *idx)
@@ -58,7 +73,7 @@ int	redirects_node(t_tree **tree, t_token *tokens, t_env_node **env_list,
 	new_node->redir = ft_strdup(tokens[*idx].str);
 	(*idx)++;
 	if (tokens[*idx].type != TOKEN_TYPE_WORD)
-		return (CODE_ERROR);
+		return (redir_error(CODE_ERROR, tokens[*idx]));
 	new_node->redir_info = extract_data(tokens[*idx].str, "", env_list);
 	new_node->origin_token = ft_strdup(tokens[*idx].str);
 	(*idx)++;
@@ -89,7 +104,11 @@ int	pipe_node(t_tree **tree, t_token *tokens, t_env_node **env_list, int idx)
 	while (tokens[idx].str && tokens[idx].type != TOKEN_TYPE_PIPE)
 		idx++;
 	if (start == idx)
+	{
+		ft_putstr_fd("jetty: syntax error near unexpected token '|'\n", 2);
+		g_exit_code = 1;
 		return (CODE_ERROR);
+	}
 	(*tree)->left = create_node(TREE_TYPE_PHRASE);
 	if (phrase_node(&((*tree)->left), tokens, env_list, start) == CODE_ERROR)
 		return (CODE_ERROR);
--- parse/parser/parser.c
+++ parse/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: erho <erho@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 16:19:03 by erho              #+#    #+#             */
-/*   Updated: 2024/05/06 18:08:16 by erho             ###   ########.fr       */
+/*   Updated: 2024/05/06 19:02:55 by erho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,15 @@ t_tree	*parse(char *str, t_env_node **env_list)
 	tokens = lexical_analyze(str);
 	if (tokens == NULL)
 		return (NULL);
+	if (tokens[0].str == NULL)
+	{
+		free_tokens(tokens);
+		return (NULL);
+	}
 	tree = create_node(TREE_TYPE_PIPE);
 	error_flag = pipe_node(&tree, tokens, env_list, 0);
 	free_tokens(tokens);
 	if (error_flag == CODE_ERROR)
-	{
-		ft_putstr_fd("jetty: syntax error near unexpected token '|'\n", 2);
-		g_exit_code = 1;
 		free_tree(&tree);
-	}
 	return (tree);
 }
